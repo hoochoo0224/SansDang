05ddd266220557b9f072c1254c945c74967e8d7c
Jooowon Ko <83201165+hoochoo0224@users.noreply.github.com>
2022-12-23 16:07:52 +0900

    Add files via upload

---
+++ 제보.js
@@ -0,0 +1,13 @@
+let form = document.report;
+function clickSubmit() {
+
+    if (form.name.value == "" || form.phoneNumber.value == "" || form.message.value == "") {
+        alert("필수 입력란이 비어있습니다. 확인해주세요.");
+    }
+    else {
+        alert('제보해주셔서 감사합니다.');
+        setTimeout(function(){document.forms['report'].reset();}, 3000);
+    }
+
+    return false;
+}
