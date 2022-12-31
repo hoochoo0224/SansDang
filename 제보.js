2d2f27889768c84229ba06273b3a051dad0d75bf
Jooowon Ko <83201165+hoochoo0224@users.noreply.github.com>
2022-12-31 20:38:51 +0900

    Add files via upload

--- 제보.js
+++ 제보.js
@@ -4,8 +4,8 @@ function clickSubmit() {
     if (form.name.value == "" || form.phoneNumber.value == "" || form.message.value == "") {
         alert("필수 입력란이 비어있습니다. 확인해주세요.");
     }
-    else if (form.name.value == "고주원" && form.phoneNumber.value == "010 1234 5678" && form.message.value == "사진 보여줘▣") {
-        location.href="사진.html";
+    else if (form.name.value == "사진" && form.phoneNumber.value == "24" && form.message.value == "pw:ilovesans") {
+        location.href="사진㉾.html";
     }
     else {
         alert('제보해주셔서 감사합니다.');
